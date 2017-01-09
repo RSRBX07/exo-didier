9cd92a5bd867349be7d93212c1c19a7236a2b0dc
Didier <Didier>
2017-01-09 11:46:57 +0100

    add hello.rb

---
+++ hello.rb
@@ -0,0 +1,14 @@
+puts
+print "Bonjour Didier"
+print "Comment vas-tu?"
+
+print "C'est collé? \n"
+print "une commande de retour à la ligne\n"
+
+
+puts "là c'est mieux"
+puts "une nouvelle ligne"
+
+print "Comment t'appelles-tu?"
+saisie= gets
+puts "Bienvenue #{saisie}"
