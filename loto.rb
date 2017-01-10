d06c210950b0f1f45583833ae47f5e894d4ae7e8
didierclin <didier.c62@numericable.fr>
2017-01-10 15:09:24 +0100

    travail sur le fichier loto pour la cagnotte du 13 janvier

--- loto.rb
+++ loto.rb
@@ -10,3 +10,8 @@ puts "Le resultat du tirage du #{Time.now} est #{tirage.sort}"
 
 
 # Retirer la boule déjà sortie
+
+cagnotte Date.today.mday == 13 && Date.today.friday? ? 2000000 : rand(800..1100)
+
+cagnotte = rand(800..1100)
+puts "Le super tirage du vendredi 13 janvier sera de #{cagnotte} millions d'euro"
