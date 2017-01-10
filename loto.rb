eaa08ef3f8eb29a21c7bce13aaca0e3f788b1a80
didierclin <didier.c62@numericable.fr>
2017-01-10 15:19:21 +0100

    Ajout ternaire pour la date

--- loto.rb
+++ loto.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 tirage = []
 boules = []
 for i in 1..45 do
@@ -11,7 +13,7 @@ puts "Le resultat du tirage du #{Time.now} est #{tirage.sort}"
 
 # Retirer la boule déjà sortie
 
-cagnotte Date.today.mday == 13 && Date.today.friday? ? 2000000 : rand(800..1100)
+cagnotte = Date.today.mday == 13 && Date.today.friday? ? 2000000 : rand(800..1100)
 
 cagnotte = rand(800..1100)
 puts "Le super tirage du vendredi 13 janvier sera de #{cagnotte} millions d'euro"
