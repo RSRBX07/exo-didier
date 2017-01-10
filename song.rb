95db0684f709ac3be03ca93c36f847d88e154d39
didierclin <didier.c62@numericable.fr>
2017-01-10 16:24:29 +0100

    New exercice : song (Ma poule)

---
+++ song.rb
@@ -0,0 +1,36 @@
+=begin
+Allongeons la jambe
+Chanson de marche
+Ma poul' n'a plus qu' vingt-neuf poussins,
+Ma poul' n'a plus qu' vingt-neuf poussins,
+Elle en avait trente.
+Allongeons la jambe,
+Allongeons la jambe, la jambe
+Car la route est longue.
+Allongeons la jambe, la jambe
+Car la route est longue.
+
+Ma poul' n'a plus qu' vingt-huit poussins,
+Ma poul' n'a plus qu' vingt-huit poussins,
+Elle en avait trente.
+Allongeons la jambe,
+Allongeons la jambe, la jambe
+Car la route est longue.
+Allongeons la jambe, la jambe
+Car la route est longue.
+
+Ma poul' n'a plus qu' vingt-sept poussins
+Ma poul' n'a plus qu' vingt-sept poussins
+Elle en avait trente.
+Allongeons la jambe,
+Allongeons la jambe, la jambe
+Car la route est longue.
+Allongeons la jambe, la jambe
+Car la route est longue.
+
+Ma poul' n'a plus qu' vingt-six poussins…25, 24, 23, 22 etc…
+
+Ma poul' n'a plus qu'un seul poussin…
+
+Ma poul' n'a plus aucun poussin…
+=end
