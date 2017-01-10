c64c90b4f3f6bdfc7392e33838eb17edec13638a
didierclin <didier.c62@numericable.fr>
2017-01-10 16:28:46 +0100

    WIP song - first implementation

--- song.rb
+++ song.rb
@@ -34,3 +34,14 @@ Ma poul' n'a plus qu'un seul poussin…
 
 Ma poul' n'a plus aucun poussin…
 =end
+puts "Allongeons la jambe
+Chanson de marche"
+
+(1..28).each do |nombre|
+  puts nombre
+  2.times {puts "Ma poul' n'a plus qu' #{30 - nombre} poussins,"}
+  puts {"Elle en avait trente.
+  Allongeons la jambe,"}
+  2.times {puts "Allongeons la jambe, la jambe
+  Car la route est longue."}
+end
