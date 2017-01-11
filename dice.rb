1bc47cf99e2427f679e198e76441e117238d566a
didierclin <didier.c62@numericable.fr>
2017-01-11 16:12:04 +0100

    Add method Roll to dice.rb

--- dice.rb
+++ dice.rb
@@ -1,9 +1,15 @@
+def roll
+  return rand 6
+end
+
+
+
 faces = [1,2,3,4,5,6]
 
 rep="z"
 while rep != "n" do
 
-  puts "lancé : #{faces[rand 6]}"
+  puts "lancé : #{faces[roll]}"
 
   puts  "lancé via tableau  #{faces[rand 6]}"
 
