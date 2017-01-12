3f89058f8c436c18cd61c582d3e2939475e1a671
didierclin <didier.c62@numericable.fr>
2017-01-12 14:36:32 +0100

    Corrected missing puts for else case statement

--- int_to_word.rb
+++ int_to_word.rb
@@ -1,12 +1,30 @@
-puts "Entrer un nombrethen "
-case gets.to_i
-  when 1 then puts "un"
-  when 2 then puts "deux"
-  when 3 then puts "trois"
-  when 4 then puts "quatre"
-  when 5 then puts "cinq"
-  when 6 then puts "six"
-  when 7 then puts "sept"
-  when 8 then puts "huit"
-  when 9 then puts "neuf"
-  else "On vous a demandé un chiffre non?"
+# ecrire une methode
+def game
+  #qui prend une instance de fixnum en argument
+  puts "Entrer un nombre "
+  #qui retourne la chaine de caracteres du chiffre en toutes lettres
+  number = gets.to_i
+  case number
+    when 1
+       "un"
+    when 2
+       "deux"
+    when 3
+       "trois"
+    when 4
+       "quatre"
+    when 5
+       "cinq"
+    when 6
+       "six"
+    when 7
+       "sept"
+    when 8
+       "huit"
+    when 9
+       "neuf"
+    else
+      "On vous a demandé un chiffre non?"
+  end
+end
+puts game
