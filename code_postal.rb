52bfde20203f10bedd62444378e94670a013e484
didierclin <didier.c62@numericable.fr>
2017-01-12 12:08:46 +0100

    code_postal.rb: Use symbols in hash of zip_codes

--- code_postal.rb
+++ code_postal.rb
@@ -1,10 +1,19 @@
+# zip_code = {
+#   "roubaix" => "59100",
+#   "lille"   => "59000",
+#   "paris"   => "75000",
+#   "Angers"  => "36143"
+# }
+
+# Use of symbols
 zip_code = {
-  "roubaix" => "59100",
-  "lille"   => "59000",
-  "paris"   => "75000",
-  "Angers"  => "36143"
+  roubaix:  "59100",
+  lille:    "59000",
+  paris:    "75000",
+  Angers:   "36143"
 }
 puts "Dans quelle ville habitez-vous ?"
-city = gets.chomp
+city = gets.chomp.to_sym
 
+puts "Le code postal de #{city} est #{zip_code[city]}"
 puts "Le code postal de #{city} est #{zip_code[city]}" if zip_code[city]
