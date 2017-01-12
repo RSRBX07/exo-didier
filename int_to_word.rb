e7be6135f1f12d34ab132a7feafb94d875c4f2e8
didierclin <didier.c62@numericable.fr>
2017-01-12 17:44:27 +0100

    Pair programming - final version for int-to-word

--- int_to_word.rb
+++ int_to_word.rb
@@ -1,11 +1,36 @@
 # ecrire une methode
-def game
+def game number
   # Ajouter un tableau des correspondances
-  numb_to_words = ["zero","un","deux","trois","quatre","cinq","six","sept","huit","neuf"]
+  digits = ["","un","deux","trois","quatre","cinq","six","sept","huit","neuf","dix","onze","douze","treize","quatorze","quinze","seize"]
+  dizaines = ["","dix","vingt","trente","quarante","cinquante","soixante"]
   #qui prend une instance de fixnum en argument
-  puts "Entrer un nombre "
-  #qui retourne la chaine de caracteres du chiffre en toutes lettres
-  number = gets.to_i
-  numb_to_words[number]
+  # puts "Entrer un nombre "
+  # #qui retourne la chaine de caracteres du chiffre en toutes lettres
+  return "zero" if number == 0
+  return  digits[number] if (0..16).include? number
+
+
+  dizaine = number.to_s[0].to_i
+  unite = number.to_s[1].to_i
+
+  dizaines[dizaine] + case unite
+    when 1
+      " et " + digits[unite]
+    when 2..9
+      "-" + digits[unite]
+    else
+      ""
+    end
+
 end
-puts game
+
+puts game 5
+puts game 15
+puts game 51
+puts game 25
+puts game 05
+puts game 33
+puts game 20
+puts game 58
+puts game 65
+puts game 68
