0519f3665cf58bcff45380dbf6903dc961e81997
didierclin <didier.c62@numericable.fr>
2017-01-17 13:53:41 +0100

    add dir loto and move files

--- loto/loto.rb
+++ loto/loto.rb
@@ -4,7 +4,7 @@ class Loto
 
   def game_closed?
     # le double !! (double négation) permet de solutionner le cas ou tirage n'est pas encore defini
-    !@tirage
+    !!@tirage
   end
 
   # enregistrer la grille pour un tirage
--- loto/loto_test.rb
+++ loto/loto_test.rb
@@ -43,21 +43,20 @@ puts "Test du loto v1"
 
 my_loto = Loto.new
 grille = my_loto.get_grid
-# grille = my_loto.validate_grid grille
 
 puts "Votre grille de jeu : #{grille.sort}"
 
 if my_loto.game_closed?
+  puts "Le tirage est déjà effectué - La grille ne peut-pas etre validée."
+else
   puts "souhaitez vous valider cette grille ? (y/n)?"
   rep = gets.chomp
   my_loto.validate_grid grille unless rep == "n"
-else
-  puts "Le tirage est déjà effectué - La grille ne peut-pas etre validée."
 end
-
-puts "tirage"
+puts "tirage du loto:"
 puts my_loto.draw
 puts "grilles"
+
 #to do valider le resultat
 
 
