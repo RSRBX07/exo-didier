6dc8bb742e1268c45dedfed7afe64b89fcd1a889
didierclin <didier.c62@numericable.fr>
2017-01-18 17:06:24 +0100

    add rspec file to vehicle

---
+++ spec/vehicle_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../vehicle/vehicle.rb'
+
+RSpec.describe Vehicle do
+  it 'fait qqchose' do
+    expect(Vehicle.count).to equal 0
+    #
+  end
+
+  # it 'give a radom flash grid' do
+  #   expect(Loto.get_flash).not_to be_nil
+  #   expect(Loto.get_flash).to be_a Array
+  #   expect(Loto.get_flash.size).to equal 5
+  #   expect(Loto.get_flash).not_to eql Loto.get_flash
+  # end
+end
