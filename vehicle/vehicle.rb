8bb8879ebc1a982ac7f288f1a322003da1babf7e
didierclin <didier.c62@numericable.fr>
2017-01-19 13:47:24 +0100

    add counter value of instance of class with an external file

--- vehicle/vehicle.rb
+++ vehicle/vehicle.rb
@@ -1,5 +1,18 @@
+# Classe compteur
 class Counter
-  
+  def add_one
+    new_val = value + 1
+    File.open("./tmp/counter.txt", "w") do |counter_file|
+      counter_file.write new_val
+    end
+
+  end
+
+  def value
+    File.open("./tmp/counter.txt", "r") do |counter_file|
+      counter_file.each_line { |line| return line.to_i }
+      end
+  end
 end
 
 class Vehicle
@@ -19,7 +32,7 @@ class Vehicle
   def self.count
     # return the number of existing objects (in memory)
     # open irb et :
-
+    Counter.new.value
   end
 
 end
