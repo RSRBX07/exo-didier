882f32dc56ab0987881f5f067f203b70fe7eb2f9
didierclin <didier.c62@numericable.fr>
2017-01-23 16:06:41 +0100

    implementation of Raise runtimeerror when no ùore player to be removed

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,7 +5,9 @@ class Game < ApplicationRecord
   end
 
   def remove_player
-    return @players if @players <= 0
+    # return @players if @players <= 0
+    # raise an exception:
+    raise RuntimeError.new "No more player to be removed" if @players <= 0
     @players -= 1
   end
 
