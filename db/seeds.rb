5916980672885ab517b9c186f15441a12b5a804b
didierclin <didier.c62@numericable.fr>
2017-01-24 11:33:39 +0100

    add games with seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Game.destroy_all
+Game.create [
+  { name: "Tirage du vendredi 13 janvier" , description: "Le tirage est 12,45,33,2,9" , players: 8254898 },
+  { name: "Tirage du mercredi 11 janvier" , description: "Le tirage est 12,35,13,2,29" , players: 2514125 },
+  { name: "Tirage du vendredi 06 janvier" , description: "Le tirage est 1,45,13,2,39" , players: 5414125 }
+]
